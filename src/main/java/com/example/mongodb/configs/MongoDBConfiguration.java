8e23d4d88e9dc6867e98420ec1e11512b1419153
jeaong_jeagong@cocone.co.jp <jjkong86@gmail.com>
2020-07-31 20:49:32 +0900

    1. [추가] MongoDBConfiguration 추가, 저장 시 _class field 제외시킴

---
+++ src/main/java/com/example/mongodb/configs/MongoDBConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.mongodb.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoDBConfiguration {
+
+    private MongoDbFactory mongoDbFactory;
+    private MongoMappingContext mongoMappingContext;
+
+    public MongoDBConfiguration(MongoDbFactory mongoDbFactory, MongoMappingContext mongoMappingContext) {
+        this.mongoDbFactory = mongoDbFactory;
+        this.mongoMappingContext = mongoMappingContext;
+    }
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter() {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
+}
