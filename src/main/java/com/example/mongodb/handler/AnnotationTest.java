7560dd4ab0f23846c7c6170be76856c07f97a803
jjkong86 <jjkong86@gmail.com>
2021-02-19 22:32:51 +0900

    add annotation get value with reflection target is method

---
+++ src/main/java/com/example/mongodb/handler/AnnotationTest.java
@@ -0,0 +1,28 @@
+package com.example.mongodb.handler;
+
+import com.example.mongodb.utils.TemplateTransactional;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+
+import java.lang.reflect.Method;
+
+public class AnnotationTest {
+
+    @TemplateTransactional(value = "transactional",
+            isolation = Isolation.READ_UNCOMMITTED,
+            propagation = Propagation.REQUIRED,
+            timeout = 3,
+            readOnly = true)
+    public void testMethod(Class<?> clazz) {
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(TemplateTransactional.class)) {
+                System.out.println(method.getAnnotation(TemplateTransactional.class).value());
+                System.out.println(method.getAnnotation(TemplateTransactional.class).isolation().value());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new AnnotationTest().testMethod(AnnotationTest.class);
+    }
+}
