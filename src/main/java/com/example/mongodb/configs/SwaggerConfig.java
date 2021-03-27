2f5b1fbea47a506361295a7aade7d2c41b390a38
jjkong86 <jjkong86@gmail.com>
2021-03-27 20:25:09 +0900

    add update swagger config Could not resolve pointer: /definitions/error does not exist in document

--- src/main/java/com/example/mongodb/configs/SwaggerConfig.java
+++ src/main/java/com/example/mongodb/configs/SwaggerConfig.java
@@ -1,27 +1,39 @@
 package com.example.mongodb.configs;
 
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    TypeResolver typeResolver = new TypeResolver();
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2)
+                .alternateTypeRules(AlternateTypeRules.newRule(
+                        typeResolver.resolve(Collection.class, Instant.class),
+                        typeResolver.resolve(Collection.class, Date.class), Ordered.HIGHEST_PRECEDENCE))
+                .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.mongodb"))
                 .paths(PathSelectors.any())
                 .build().apiInfo(apiInfo())
